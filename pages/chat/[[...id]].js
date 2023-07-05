cd0db30c4bfe0b1e1a4bb989b4b3c941522905bf
Jonah Jackson <jonahjackson@uflip.com>
2023-07-05 11:42:27 -0500

    sec3 complete

--- pages/chat/[[...id]].js
+++ pages/chat/[[...id]].js
@@ -1,12 +1,40 @@
 import Head from "next/head";
+import { ChatSideBar } from "components/sidebar";
+import { useState } from "react";
 
 export default function Home() {
+  const [messageText, setMessageText] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(messageText);
+  }
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>New chat</title>
       </Head>
-      <h1>dynamic chat</h1>
-    </div>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <ChatSideBar />
+        <div className="flex flex-col bg-gray-700">
+          <div className="flex-1">chat window</div>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -19,25 +19,16 @@ export default function Home() {
       <div className="flex min-h-screen w-full items-center justify-center bg-gray-800">
         {!user && (
           <>
-            <Link
-              href="/api/auth/login"
-              className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-            >
+            <Link href="/api/auth/login" className="btn">
               Login
             </Link>
-            <Link
-              href="/api/auth/signup"
-              className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-            >
+            <Link href="/api/auth/signup" className="btn">
               Sign up
             </Link>
           </>
         )}
         {user && (
-          <Link
-            href="/api/auth/logout"
-            className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
-          >
+          <Link href="/api/auth/logout" className="btn">
             Logout
           </Link>
         )}
